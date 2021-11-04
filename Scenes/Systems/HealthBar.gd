64d34da1b90dc17dfa1a2113ec2a126be785f215
Guillaume Olivier <guillaume.olivier@epitech.eu>
2021-11-04 19:22:05 +0900

    Revert "Revert "Revert "[ADD] - Player stats and healthbar connected"""
    This reverts commit 25363522c8296440e6ebb22b1ced0e78971f91b9.

--- Scenes/Systems/HealthBar.gd
+++ Scenes/Systems/HealthBar.gd
@@ -1,25 +1,29 @@
 extends Control
 
 const MIN_HEALTH: int = 17
-const MAX_HEALTH: int = 50
+const MAX_HEALTH: int = 83
 
-var currentHealth: int = 0
+var currentHealth: int = 0 
+var maxHpPool: int = 83
 
 # Called when the node enters the scene tree for the first time.
-onready var player: KinematicBody2D = get_parent().get_parent().get_node("Player")
-onready var healthBarOver: TextureProgress = get_node("HealthBarOver")
-onready var healthBarUnder: TextureProgress = get_node("HealthBarUnder")
+onready var player: KinematicBody2D = get_parent().get_node("Player")
+onready var healthBar: TextureProgress = get_node("HealthBar")
 onready var healthBarTween: Tween = get_node("HealthBar/Tween")
 
+func _ready() -> void:
+	maxHpPool = player.HEALTH
+	print(maxHpPool)
+	_updateHealthBar(40)
+
 func _updateHealthBar(newHealthValue: int) -> void:
-	healthBarOver.value = newHealthValue
-	var __ = $HealthBarOver/Tween.interpolate_property(healthBarUnder, "value", healthBarUnder.value , newHealthValue, 0.25, Tween.TRANS_SINE , Tween.EASE_OUT, 0.25)
-	__ = $HealthBarOver/Tween.start()
+	healthBar.value = newHealthValue
+	var __ = healthBarTween.interpolate_property($healthBar, "value", newHealthValue, 0.25, Tween.TRANS_QUINT, Tween.EASE_OUT)
+	__ = healthBarTween.start()
 
 func _on_Player_hp_changed(newHp: int) -> void:
 	var newHealth: int = int(MAX_HEALTH - MIN_HEALTH) * float(newHp) / MAX_HEALTH + MIN_HEALTH
 	print(newHealth)
 	_updateHealthBar(newHealth)
-
 #func _process(delta: float) -> void:
 #	pass
