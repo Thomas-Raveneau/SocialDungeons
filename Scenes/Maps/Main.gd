fb31cfe0fa3fd9eeeafa50337e179c12ec317996
Guillaume Olivier <guillaume.olivier@epitech.eu>
2021-11-06 11:22:23 +0900

    [ADD] - New assets turrets / projectiles

--- Scenes/Maps/Main.gd
+++ Scenes/Maps/Main.gd
@@ -15,8 +15,6 @@ func _ready() -> void:
 
 func _process(_delta: float) -> void:
 	handle_player_room_exit()
-	if (Input.is_action_just_pressed("debug_button")):
-		player.damage(10, Vector2(0, 200))
 
 func set_player_to_spawn() -> void:
 	player.position = player_spawn.position
