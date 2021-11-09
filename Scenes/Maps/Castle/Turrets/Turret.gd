b4d5e77ec977ece2ee62b96a58e511cb94c90beb
Thomas-Raveneau <thomas.raveneau1@gmail.com>
2021-11-09 11:47:03 +0900

    [FIX] - add offset on spawning position of turret's arrow

--- Scenes/Maps/Castle/Turrets/Turret.gd
+++ Scenes/Maps/Castle/Turrets/Turret.gd
@@ -3,34 +3,40 @@ extends StaticBody2D
 ################################################################################
 
 # STATS
-export var BULLET_SPEED: float = 10.0
-export var BULLET_FREQUENCY: float = 1.0
+export var ARROW_SPEED: float = 10.0
+export var ARROW_FREQUENCY: float = 1.0
 
 # NODES
 onready var shoot_timer: Timer = $ShootTimer
 onready var sprite: AnimatedSprite = $TurretSprite
 
 # SCENES
-onready var Bullet = preload("res://Scenes/Maps/Castle/Turrets/Bullet.tscn")
+onready var Arrow = preload("res://Scenes/Maps/Castle/Turrets/Arrow.tscn")
+
+# UTILS
+var arrow_offset: int = 25
 
 ################################################################################
 
 ### PRIVATE ###
 func _ready() -> void:
-	shoot_timer.wait_time = BULLET_FREQUENCY
+	shoot_timer.wait_time = ARROW_FREQUENCY
 	shoot_timer.start()
+	rotation_degrees = round(rotation_degrees)
 
-func _shoot_bullet() -> void:
-	var bullet = Bullet.instance()
-	var bullet_dir: Vector2 = Vector2(cos(rotation), sin(rotation))
+func _shoot_arrow() -> void:
+	var arrow = Arrow.instance()
+	var arrow_dir: Vector2 = Vector2(cos(rotation), sin(rotation))
 	
-	bullet.set_speed_and_dir(BULLET_SPEED, bullet_dir)
-	add_child(bullet)
+	arrow.position.x += arrow_offset 
+	arrow.set_speed_and_dir(ARROW_SPEED, arrow_dir)
+	
+	add_child(arrow)
 
 ### SIGNALS ###
 func _on_Timer_timeout() -> void:
 	sprite.play("shoot")
-	_shoot_bullet()
+	_shoot_arrow()
 
 func _on_TurretSprite_animation_finished():
 	sprite.stop()
