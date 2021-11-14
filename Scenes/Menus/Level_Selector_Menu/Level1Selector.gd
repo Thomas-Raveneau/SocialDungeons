43ea07e7e7d5024c00c96250ab29b48dc077f21c
Guillaume Olivier <guillaume.olivier@epitech.eu>
2021-11-15 00:14:15 +0900

    [ADD] - Basic highscore save

---
+++ Scenes/Menus/Level_Selector_Menu/Level1Selector.gd
@@ -0,0 +1,16 @@
+extends TextureButton
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	get_node("Label").set_text(str(Save.currentHighScore))
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
