6391f866ef04efa34a8de18d30a36f3ed68a400e
nautiii <quentin.maillard@epitech.eu>
2021-11-18 18:09:04 +0900

    [Add] - hole functional

--- Scenes/Maps/Castle/Traps/Hole.gd
+++ Scenes/Maps/Castle/Traps/Hole.gd
@@ -1,20 +1,29 @@
 extends Area2D
 
 # STATS
-export var DAMAGE: float = 15
+export var DAMAGE: float = 20
 
 # UTILS
-var player_node: Node
-var is_hole_active: bool = true
+var player: Node = null
+var player_size: Vector2 = Vector2.ZERO
 
 func _ready():
 	pass
 
-func _on_Hole_body_entered(body):
-	if (get_tree().get_nodes_in_group("player").has(body) and is_hole_active):
-		body.damage(DAMAGE, Vector2.ZERO)
-		is_hole_active = false
-		$FallCooldown.start()
+func _physics_process(_delta):
+	if (player):
+		is_player_inside()
 
-func _on_FallCooldown_timeout():
-	is_hole_active = true
+func is_player_inside() -> void:
+	if (player.position.y + player_size.y < position.y):
+		player.fall()
+		player = null
+
+func _on_Hole_body_entered(body):
+	if (get_tree().get_nodes_in_group("player").has(body)):
+		player = body
+		player_size = player.skin.get_sprite_frames().get_frame("idle", 0).get_size()
+
+func _on_Hole_body_exited(body):
+	if (get_tree().get_nodes_in_group("player").has(body)):
+		player = null
