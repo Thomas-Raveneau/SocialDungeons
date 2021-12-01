ccac9d6ddf38e062211577ac0ec4b592ed0c8ca8
guillaumechapau <guillaume.chapau@epitech.eu>
2021-12-02 00:34:21 +0900

    début d'anim user feedback

--- Scenes/UI/ButtonBar.gd
+++ Scenes/UI/ButtonBar.gd
@@ -5,15 +5,24 @@ signal first_spell(cooldown)
 signal second_spell(cooldown)
 signal ultimate(cooldown)
 
+export (float) var shader_range = 0.0;
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func update_shader_range():
+	$FirstSpellFlash.material.set_shader_param("range", shader_range);
+
+func animate():
+	$AnimationPlayer.play("spellFlash");
 
 func _on_Player_spell(spellNumber: int, spellCooldown: float) -> void:
 	if spellNumber == 1:
 		emit_signal("autohit", spellCooldown);
 	elif spellNumber == 2:
-		emit_signal("first_spell", spellCooldown);
+		animate();
+		#emit_signal("first_spell", spellCooldown);
 	elif spellNumber == 3:
 		emit_signal("second_spell", spellCooldown);
 	else:
