21285503589fe0e8178824f76ee08906275dc2b7
nautiii <quentin.maillard@epitech.eu>
2021-12-08 13:52:57 +0900

    [Add] - fireball particles

--- Scenes/Projectile/FireBall.gd
+++ Scenes/Projectile/FireBall.gd
@@ -1,5 +1,8 @@
 extends "res://Scenes/Projectile/AProjectile.gd"
 
+# SCENES
+var explosion_particles = preload("res://Scenes/Particles/Explosion.tscn")
+
 ############################# PRIVATE METHODS ##################################
 
 func _ready():
@@ -14,10 +17,17 @@ func _handle_movement():
 	velocity = orientation.normalized() * SPEED
 	velocity = move_and_slide(velocity)
 
+func _generate_explosion() -> void:
+	var explosion = explosion_particles.instance()
+	explosion.global_position = Vector2(global_position.x, global_position.y)
+	explosion.direction = orientation
+	get_parent().add_child(explosion)
+
 func _handle_collision():
 	._handle_wall_collision()
 	var slide_count = get_slide_count()
 	for i in slide_count:
+		_generate_explosion()		
 		var collided_node = get_slide_collision(i)
 		if (get_tree().get_nodes_in_group("player").has(collided_node.collider)):
 			collided_node.collider.damage(DAMAGE, orientation)
